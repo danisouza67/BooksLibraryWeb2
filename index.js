2f4c473134e98a9baf2cc7fd80b3e11009182d08
danisouzahigh <daniel.araujo.web@hotmail.com>
2021-04-14 00:26:52 +0000

    ver 1.1

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const axios = require("axios").default;
 //create a server object:
 http
   .createServer(function(req, res) {
-    res.write(users.join("\n")); //display the list of users on the page
+    res.write( users.join("\n")); //display the list of users on the page
+    res.write("\n\n" + emails.join("\n"))
     res.end(); //end the response
   })
   .listen(8080); //the server object listens on port 8080
 
-let users = [];
+let users, emails = [];
 
 (async function getNames() {
   try {
@@ -17,7 +18,10 @@ let users = [];
       "https://jsonplaceholder.typicode.com/users"
     );
     users = data.map(user => user.name);
+    emails = data.map(email => email.email)
   } catch (error) {
     console.log(error);
   }
 })();
+
+// app.use(logger('dev'));
