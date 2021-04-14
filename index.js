f853241d281d1f7a447a0150ab06effa3e0a7f22
danisouzahigh <daniel.araujo.web@hotmail.com>
2021-04-14 01:25:57 +0000

    vre1.3

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
-var http = require("http");
-const axios = require("axios").default;
+const http = require("http"),
+axios = require("axios"), 
+logger = require("morgan"),
+cors = require("cors"),
+express = require("express"),
+bodyParser = require("body-parser");
 
+var app = express();
+var port = 8000;
+
+
+app.use(bodyParser.json())
+
+app.get("/hello/:foo/:bar", (req, res) =>{
+    res.json({message: "Books Library", data: [
+        req.params.foo,
+        req.params.bar
+    ]});
+    // res.send(user.join("\n"))
+
+});
 //create a server object:
-http
-  .createServer(function(req, res) {
-    res.write( users.join("\n")); //display the list of users on the page
-    res.write("\n\n" + emails.join("\n"))
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+// http
+//   .createServer(function(req, res) {
+//     //   users.forEach(users => {
+//     //       res.write( "Name: " + users.join("\n"));
+//     //   });
+//     res.write( "Name: " + users.join("\n")); //display the list of users on the page
+//     res.write("\n\nMass: " + mass.join("\n"))
+//     res.end(); //end the response
+//   })
+//   .listen(8080); //the server object listens on port 8080
 
-let users, emails = [];
+let users, mass = [];
 
 (async function getNames() {
   try {
     const { data } = await axios.get(
-      "https://jsonplaceholder.typicode.com/users"
+      "https://swapi.dev/api/people/"
     );
-    users = data.map(user => user.name);
-    emails = data.map(email => email.email)
+    users = data.results.map(char => char.name);
+    mass = data.results.map(char => char.mass)
   } catch (error) {
     console.log(error);
   }
 })();
 
+app.listen(port, (err) => {
+    console.log("Listening on port: " + port);
+})
 // app.use(logger('dev'));
